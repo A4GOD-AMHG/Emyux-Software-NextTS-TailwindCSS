6a656efcd9e8a2df3bf342afe6e0ba456149f56c
a4god- <alexismhgarcia@gmail.com>
2025-05-09 02:35:14 -0400

    added new section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,12 +3,14 @@ import ImprovedCTA from '@/components/ImprovedCTA';
 import HeroSection from '@/components/HeroSection';
 import ServicesSection from '@/components/ServicesSection';
 import ProblemsSection from '@/components/ProblemsSection';
+// import AboutSection from '@/components/AboutSection';
 
 export default function Home() {
 
     return (
-        <main className="max-w-7xl flex flex-col grow mx-auto px-4 sm:px-6 lg:px-8">
+        <main className="max-w-7xl not-first:flex flex-col grow mx-auto px-4 sm:px-6 lg:px-8">
             <HeroSection />
+            {/* <AboutSection /> */}
             <ProblemsSection />
             <ServicesSection />
             <TestimonialsCarousel />
